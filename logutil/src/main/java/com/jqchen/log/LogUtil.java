e6776248960252164100b9a70811ac99d4351fec
PAXDATA\Jesse.C <JesseChen@paxsz.com>
2022-03-07 15:51:50 +0800

    delete copyright.

--- logutil/src/main/java/com/jqchen/log/LogUtil.java
+++ logutil/src/main/java/com/jqchen/log/LogUtil.java
@@ -1,19 +1,3 @@
-/*
- * ===========================================================================================
- *    COPYRIGHT
- *             PAX Computer Technology(Shenzhen) CO., LTD PROPRIETARY INFORMATION
- *      This software is supplied under the terms of a license agreement or nondisclosure
- *      agreement with PAX Computer Technology(Shenzhen) CO., LTD and may not be copied or
- *      disclosed except in accordance with the terms in that agreement.
- *        Copyright (C) 2018 - ? PAX Computer Technology(Shenzhen) CO., LTD All rights reserved.
- *    Description: // Detail description about the function of this module,
- *                // interfaces with the other modules, and dependencies.
- *    Revision History:
- *    Date	                 Author	                Action
- *    2019-05-05   	         JQChen           	Create/Add/Modify/Delete
- *   ===========================================================================================
- */
-
 package com.jqchen.log;
 
 import android.content.ClipData;
